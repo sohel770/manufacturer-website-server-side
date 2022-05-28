2f8f285935d4bd6129ffc8145c4f331f18a2dce8
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-28 11:19:45 +0600

    commit

--- index.js
+++ index.js
@@ -15,75 +15,142 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-
-    try {
-
-        await client.connect();
-        const serviceCollection = client.db("carBiz").collection("service");
-        const ratingCollection = client.db("carRating").collection("rating");
-        const orderCollection = client.db("carOrder").collection("order");
-
-
-        app.post('/service', async (req, res) => {
-            const service = req.body;
-            const result = await serviceCollection.insertOne(service);
-            res.json(result);
-        })
-
-        // Rating (POST)
-        app.post("/rating", async (req, res) => {
-            const rating = req.body;
-            const result = await ratingCollection.insertOne(rating);
-            res.json(result);
-
-        })
-
-        // Orders post
-        app.post("/myOrders", async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            console.log(result);
-            res.send(result);
-
-        })
-
-        // ........................Read(R):(GET).................................//
-        // Read (GET)
-        app.get("/service", async (req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray()
-            res.send(services)
-        })
+try{
+    
+    await client.connect();
+    console.log('mediTools db connected');
+    const toolCollection = client.db('mediToolsDB').collection('product');
+    const reviewCollection = client.db('mediToolsDB').collection('reviews');
+    const orderColletion = client.db('mediToolsDB').collection('orders');
+    const userColletion = client.db('mediToolsDB').collection('users');
 
 
-
-        // Order get
-        app.get("/myOrders/:email", async (req, res) => {
-            console.log(req.params.email);
-            const cursor = orderCollection.find({ email: req.params.email });
-            const result = await cursor.toArray();
-            res.send(result);
-        });
-
-        // Rating
-        app.get("/rating", async (req, res) => {
-            const query = {}
-            const cursor = ratingCollection.find(query);
-            const ratings = await cursor.toArray()
-            res.send(ratings)
-        })
-        // Get Single service
-        app.get("/service/:id", async (req, res) => {
-            const id = req.params;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
-
-
-
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+        const requester = req.decoded.email;
+        const requesterAccount = await userColletion.findOne({ email: requester });
+        if (requesterAccount.role === 'admin') {
+            next();
+        }
+        else {
+            res.status(403).send({ message: 'Forbidden' });
+        }
     }
+
+
+    // tools product api
+    app.post('/product', async (req, res) => {
+        const query = req.body;
+        const product = await toolCollection.insertOne(query);
+        res.send(product);
+    });
+
+    // get product api from database
+    app.get('/product', async (req, res) => {
+        const query = {};
+        const product = await toolCollection.find(query).toArray();
+        res.send(product);
+    });
+
+
+    // delete product api
+    app.delete('/product/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const result = await toolCollection.deleteOne(filter);
+        res.send(result);
+    });
+
+    // get product api by id from database
+    app.get('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const product = await toolCollection.findOne(query);
+        res.send(product);
+    });
+
+    // post review api
+    app.post('/review', async (req, res) => {
+        const query = req.body;
+        const review = await reviewCollection.insertOne(query);
+        res.send(review);
+    });
+
+    // get review api
+    app.get('/review', async (req, res) => {
+        const query = {};
+        const review = await reviewCollection.find(query).toArray();
+        res.send(review);
+    });
+
+    // order post api
+    app.post('/order', async (req, res) => {
+        const order = req.body;
+        const result = await orderColletion.insertOne(order);
+        res.send(result);
+    });
+
+    app.get('/order', verifyJWT, async (req, res) => {
+        const query = {};
+        const orders = await orderColletion.find(query).toArray();
+        res.send(orders);
+    });
+
+    // order collection by email
+    app.get('/order', verifyJWT, async (req, res) => {
+        const customer = req.query.customer;
+        const decodedEamil = req.decoded.email;
+        if (customer === decodedEamil) {
+            const query = { customer: customer };
+            const orders = await orderColletion.find(query).toArray();
+            return res.send(orders);
+        }
+        else {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+
+    });
+
+    // laod users inforamtion on dashboard
+    app.get('/user', verifyJWT, async (req, res) => {
+        const users = await userColletion.find().toArray();
+        res.send(users);
+    })
+
+    app.get('/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await userColletion.findOne({ email: email });
+        const isAdmin = user.role === 'admin';
+        res.send({ admin: isAdmin });
+    })
+
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const updateDoc = {
+            $set: { role: 'admin' },
+        };
+        const result = await userColletion.updateOne(filter, updateDoc);
+        res.send(result);
+    });
+
+
+    // update user information api
+    app.put('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: user,
+        };
+        const result = await userColletion.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        res.send({ result, token });
+    });
+
+}
+    
 
 
     finally {
